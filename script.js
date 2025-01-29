0e032c67e0c513fd01e8a35d97882007712e1271
Nannasarah <nannasp01@gmail.com>
2025-01-29 18:49:38 +0100

    ovelse2+3done

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ function tjek() {
     console.log("alkoholfri");
   }
 }
+
+function hilsen() {
+  const tid = new Date().getHours();
+  if (tid < 5) {
+    console.log("Godnat");
+  } else if (tid < 10) {
+    console.log("Godmorgen");
+  } else if (tid < 18) {
+    console.log("Goddag");
+  } else {
+    console.log("Godaften");
+  }
+}
+hilsen();
+
+document.querySelector("button").addEventListener("click", tjekTal);
+let forsoeg = 0;
+const ranNum = Math.floor(Math.random() * 11);
+
+function tjekTal() {
+  forsoeg++;
+  const brugerTal = document.querySelector("#tal").value;
+
+  if (ranNum > brugerTal) {
+    alert("Øv! " + brugerTal + " var for lavt. Prøv igen :)");
+  } else if (ranNum < brugerTal) {
+    alert("Øv! " + brugerTal + " var for højt. Prøv igen :)");
+  } else {
+    alert(brugerTal + " er rigtigt!" + " Du brugte " + forsoeg + " forsøg. Prøv igen med et nyt tal :)");
+    location.reload();
+  }
+}
